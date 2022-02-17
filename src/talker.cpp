0284d5e16e114420c3829e7fe6dd7432d880b6d3
jaeu-choe <jaeuchoe@snu.ac.kr>
2022-02-17 17:30:01 +0900

    pub & sub practice

--- src/talker.cpp
+++ src/talker.cpp
@@ -8,9 +8,9 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
 
-    ros::Publisher chatter_pub = n.advertise<std_msgs::String("chatter", 1000);
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
-    ros::Rate loop_rate(10)
+    ros::Rate loop_rate(10);
     
     int count = 0;
     while (ros::ok())
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 
         std::stringstream ss;
         ss << "hello world " << count;
-        msg.data == ss.str();
+        msg.data = ss.str();
 
         ROS_INFO("%s", msg.data.c_str());
 
